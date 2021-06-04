8a6f9b944520eaaa398121b8e435ef1c2c24e88e
Aman Raj Singhania <84716879+aman-raj-singhania@users.noreply.github.com>
2021-06-04 13:55:02 +0530

    my2_wallis.c

--- wallis.c
+++ wallis.c
@@ -5,27 +5,13 @@
 
 float wallis_pi(int n)
 {
-  float num,num2,num3,mypi;
-  if (n==0)
+  float j;
+  float mypi=4;
+  
+  for (j=3;j<n+2;j+=2)
   {
-    return 0;
+    mypi = mypi * ((j-1)/j) * ((j+1)/j);
   }
-  for (int j=1;j<=n;j++)
-  {
-    num = 4*j*j;
-    num2 = num-1;
-    num3 = num/num2;
-    
-    if (j==1)
-    {
-      mypi = num3;
-    }
-    else
-    {
-      mypi = mypi * num3;
-    }
-  }
-  mypi *= 2;
   return mypi;
 }
 
