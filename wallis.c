5d8cf4740f1a6d0c07dbf7edd6da20d4e0569436
Aman Raj Singhania <84716879+aman-raj-singhania@users.noreply.github.com>
2021-06-04 12:47:16 +0530

    test_wallis.c

--- wallis.c
+++ wallis.c
@@ -3,7 +3,31 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
+float wallis_pi(int n)
+{
+  float num,num2,num3,mypi;
+  if (n==0)
+  {
+    return 0;
+  }
+  for (int j=1;j<=n;j++)
+  {
+    num = 4*j*j;
+    num2 = num-1;
+    num3 = num/num2;
+    
+    if (j==1)
+    {
+      mypi = num3;
+    }
+    else
+    {
+      mypi = mypi * num3;
+    }
+  }
+  mypi *= 2;
+  return mypi;
+}
 
 int main(void) {
   float pi;
