2c3f5f8ce782fc81b8432485feda0c0595115ac0
Aman Raj Singhania <84716879+aman-raj-singhania@users.noreply.github.com>
2021-06-04 13:31:47 +0530

    my_montecarlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,12 +3,27 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
+}
+
+float mc_pi(int n)
+{
+  int count=0;
+  for (int i=0;i<n;i++)
+  {
+    float x = frandom();
+    float y = frandom();
+    float z = x*x + y*y ;
+    
+    if (z<=1)
+    {
+      count++;
+    }
+  }
+  return 4*count/n;
 }
 
 int main(void) {
